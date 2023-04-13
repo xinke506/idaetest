47969bf9fb03ac7e58bbe6ac4c66de336d9b86ec
xiaobao <xiaobao@qq.com>
2023-04-13 17:51:22 +0800

    代码进行了改造

--- src/main/java/com/xinke/controller/Controller01.java
+++ src/main/java/com/xinke/controller/Controller01.java
@@ -7,6 +7,7 @@ public class Controller01 {
     @RequestMapping("/show")
     public String show(){
         System.out.println("调用了Controller01 的show方法~！");
+        System.out.println("新添加了点东西");
         return "success";
     }
 }
